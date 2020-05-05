990ef427b7a0468785e845aabb41bf9f1fe00675
Juliana Karoline <julianaklulo@gmail.com>
2020-05-05 20:24:07 -0300

    Create Author and Book models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=150)
+    publication_year = models.PositiveSmallIntegerField()
+    edition = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
