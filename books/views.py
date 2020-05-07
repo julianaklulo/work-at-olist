df47ea8c1cdc46fffb203ae1d5a42f788105e076
Juliana Karoline <julianaklulo@gmail.com>
2020-05-07 04:24:04 -0300

    Create views for CRUD operations for Author and Book models

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import AuthorSerializer, BookSerializer
+from .models import Author, Book
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('name')
+    serializer_class = BookSerializer
