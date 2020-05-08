9579784894513126b98acb8701b4b7c0b55bc815
Juliana Karoline <julianaklulo@gmail.com>
2020-05-08 04:04:07 -0300

    Add filtering options on Author and Book endpoints

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.response import Response
 
 from .serializers import AuthorSerializer, BookSerializer
@@ -8,8 +8,12 @@ from .models import Author, Book
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('name')
     serializer_class = AuthorSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('name')
     serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'publication_year', 'edition', 'authors__name']
--- library/settings.py
+++ library/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
