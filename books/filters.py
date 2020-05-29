862bdaa358c979b8b44799ff848a424ac2d87734
Juliana Karoline <julianaklulo@gmail.com>
2020-05-29 20:16:41 -0300

    Create filters for Book and Author models

---
+++ books/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters
+from .models import Book
+
+
+class AuthorFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr="contains")
+
+
+class BookFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr="contains")
+    publication_year = filters.NumberFilter(lookup_expr="exact")
+    edition = filters.CharFilter(lookup_expr="contains")
+    authors__name = filters.CharFilter(lookup_expr="contains")
