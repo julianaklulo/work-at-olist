3f8a33f4abddf0c782611c93a125574b520bdaf4
Juliana Karoline <julianaklulo@gmail.com>
2020-05-29 20:17:58 -0300

    Use customized filters for Author and Book endpoints

--- books/views.py
+++ books/views.py
@@ -1,19 +1,18 @@
-from rest_framework import viewsets, filters
-from rest_framework.response import Response
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import AuthorSerializer, BookSerializer
 from .models import Author, Book
+from .filters import AuthorFilter, BookFilter
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all().order_by('name')
     serializer_class = AuthorSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name']
+    filterset_class = AuthorFilter
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('name')
     serializer_class = BookSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'publication_year', 'edition', 'authors__name']
+    filterset_class = BookFilter
