f3b1ea85b9395c7334662e49f6fb76a1fd8b0904
Juliana Karoline <julianaklulo@gmail.com>
2020-05-29 21:21:56 -0300

    Override list and retrive methods to use new book read serializer

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 
-from .serializers import AuthorSerializer, BookSerializer
+from .serializers import AuthorSerializer, BookSerializer, BookReadSerializer
 from .models import Author, Book
 from .filters import AuthorFilter, BookFilter
 
@@ -16,3 +18,13 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('name')
     serializer_class = BookSerializer
     filterset_class = BookFilter
+
+    def list(self, request):
+        queryset = Book.objects.all().order_by('name')
+        serializer = BookReadSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        book = get_object_or_404(Book.objects.all(), pk=pk)
+        serializer = BookReadSerializer(book)
+        return Response(serializer.data)
